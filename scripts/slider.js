fee92fc43c31d45cdc1b52b76baa65e0068ec160
Sky SeVen <shrikelionel0@gmail.com>
2023-03-31 21:12:56 +0300

    Информация в patchnote.md.

---
+++ scripts/slider.js
@@ -0,0 +1,37 @@
+let activeSlide = 0;
+const slides = document.getElementsByClassName("slide");
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+
+console.log(slides);
+
+document.addEventListener("DOMContentLoaded", function (){
+  setTimeout(showSlide(0), 5000);
+});
+function showSlide(n) {
+    slides[n].classList.add("active");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    slides[n].classList.add("active");
+}
+
+
+prevBtn.addEventListener('click', function() {
+    if (activeSlide === 0) {
+        activeSlide = slides.length - 1;
+    } else {
+        activeSlide--;
+    }
+    showSlide(activeSlide);
+});
+
+nextBtn.addEventListener("click", function() {
+    if (activeSlide === slides.length - 1) {
+        activeSlide = 0;
+    } else {
+        activeSlide++;
+    }
+    showSlide(activeSlide);
+});
